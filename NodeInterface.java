49b592ebced56dd3f8169ce72f90e19d39ecd9c2
elaylevy123 <119055463+elaylevy123@users.noreply.github.com>
2023-03-06 18:25:13 +0200

    Update NodeInterface.java

--- NodeInterface.java
+++ NodeInterface.java
@@ -3,7 +3,7 @@ package GameEx_Final;
  * This interface represents a single node in a LinkedList which holds a GeoShape object as the data,
  * and NodeInterface as the next node.
  * The methods includes: get/set the data, get/set the next node.
- * @author Asaly.Saed
+ * @author elay levi
  *
  */
 public interface NodeInterface {
