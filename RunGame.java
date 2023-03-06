8edd45e700eecf39c044e00630f8a2841cddab49
elaylevy123 <119055463+elaylevy123@users.noreply.github.com>
2023-03-06 18:24:37 +0200

    Update RunGame.java

--- RunGame.java
+++ RunGame.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 /**
  * This is the main class to be run in order to play the game. 
  *  Ex4: you should NOT change this class!
- * @author Asaly.Saed
+ * @author elay levi
  */
 public class RunGame {
 	 static JFrame frame = new JFrame("Popcorn Game");
